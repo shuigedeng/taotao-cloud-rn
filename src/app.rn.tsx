bd95fcaa4df2e4a268c1ad17a112ea43672ac449
shuigedeng <981376577@qq.com>
2021-12-03 22:06:17 +0800

    [taotao-cloud-project-1340] update[taotao-cloud-starter oss gateway]

---
+++ src/app.rn.tsx
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+import './app.less'
+import {Provider} from 'react-redux'
+import configStore from './store'
+
+const store = configStore()
+
+class App extends Component {
+
+  componentDidMount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  componentDidCatchError() {
+  }
+
+  // this.props.children 是将要会渲染的页面
+  render() {
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export default App
